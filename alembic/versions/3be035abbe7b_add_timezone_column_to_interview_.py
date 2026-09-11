4629ec904ab50d6a3a0b0f6ebc648ccaf4d45def
Kartikay Kumar <kartikaykumar2812@gmail.com>
2026-09-11 08:08:00 +0000

    style: black format new migration file

--- alembic/versions/3be035abbe7b_add_timezone_column_to_interview_.py
+++ alembic/versions/3be035abbe7b_add_timezone_column_to_interview_.py
@@ -5,25 +5,25 @@ Revises: ba859ad28cca
 Create Date: 2026-09-11 08:00:57.572840
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
-revision: str = '3be035abbe7b'
-down_revision: Union[str, Sequence[str], None] = 'ba859ad28cca'
+revision: str = "3be035abbe7b"
+down_revision: Union[str, Sequence[str], None] = "ba859ad28cca"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     op.add_column(
-        'interview_schedules',
-        sa.Column('timezone', sa.String(64), nullable=False, server_default='UTC')
+        "interview_schedules",
+        sa.Column("timezone", sa.String(64), nullable=False, server_default="UTC"),
     )
 
 
 def downgrade() -> None:
-    op.drop_column('interview_schedules', 'timezone')
+    op.drop_column("interview_schedules", "timezone")
